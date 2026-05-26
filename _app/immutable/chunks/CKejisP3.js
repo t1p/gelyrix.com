12dd49dc8f05be4fa8a823e985e89231b24e5f9a
TIP <trapeznikovip@gmail.com>
2026-05-26 11:02:35 +0200

    Deploy static build with sitemap.xml

--- _app/immutable/chunks/CKejisP3.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1hs1pxa?.base??"/gelyrix.com",a=globalThis.__sveltekit_1hs1pxa?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/CXc4rSiv.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1ga306w?.base??"/gelyrix.com",a=globalThis.__sveltekit_1ga306w?.assets??s??"";export{a,s as b};
