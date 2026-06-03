f7b021317ee747a5eba4b2a0939067fef6e51332
TIP <trapeznikovip@gmail.com>
2026-06-03 14:20:09 +0200

    fix: update GitHub Pages static build

--- _app/immutable/chunks/CXc4rSiv.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1ga306w?.base??"/gelyrix.com",a=globalThis.__sveltekit_1ga306w?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/wzZS83QQ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1b1x5xw?.base??"/gelyrix.com",e=globalThis.__sveltekit_1b1x5xw?.assets??s??"";export{e as a,s as b};
