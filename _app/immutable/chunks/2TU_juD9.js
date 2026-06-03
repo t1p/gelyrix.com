2ff73c2b7147cea5718255cbbb7c7116e496f5d3
TIP <trapeznikovip@gmail.com>
2026-06-03 15:18:07 +0200

    Publish GitHub Pages locale switcher fix

---
+++ _app/immutable/chunks/2TU_juD9.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1iujej9?.base??"/gelyrix.com",e=globalThis.__sveltekit_1iujej9?.assets??s??"";export{e as a,s as b};
--- _app/immutable/chunks/wzZS83QQ.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1b1x5xw?.base??"/gelyrix.com",e=globalThis.__sveltekit_1b1x5xw?.assets??s??"";export{e as a,s as b};
